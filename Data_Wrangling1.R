870b15bb81cef3260c5c7da74cc537ca026e0d3e
Rochelley <rochelle.maggie@gmail.com>
2016-10-10 14:49:40 -0600

    Add files via upload

--- Data_Wrangling1.R
+++ Data_Wrangling1.R
@@ -53,15 +53,19 @@ my_data <- my_data %>%
   mutate (full_address = paste(address, city, country, sep= ", "))
 
 # 5. Create dummy variables for company and product
-my_data <- my_data %>% rowwise() %>% mutate(company_philips = ifelse(company == 'philips',1,0),
-                                      company_akzo = ifelse(company == 'akzo',1,0),
-                                      company_van_houten = ifelse(company == 'van houten',1,0),
-                                      company_unilever = ifelse(company == 'unilever',1,0))
+my_data <- my_data %>% 
+  rowwise() %>% 
+  mutate(company_philips = ifelse(company == 'philips',1,0),
+        company_akzo = ifelse(company == 'akzo',1,0),
+        company_van_houten = ifelse(company == 'van houten',1,0),
+        company_unilever = ifelse(company == 'unilever',1,0))
 
-my_data <- my_data %>% rowwise() %>% mutate(product_smartphone = ifelse(product_code == 'p',1,0),
-                                      product_tv = ifelse(product_code == 'v',1,0),
-                                      product_laptop= ifelse(product_code == 'x',1,0),
-                                      product_tablet = ifelse(product_code == 'q',1,0))
+my_data <- my_data %>% 
+  rowwise() %>% 
+  mutate(product_smartphone = ifelse(product_code == 'p',1,0),
+        product_tv = ifelse(product_code == 'v',1,0),
+        product_laptop= ifelse(product_code == 'x',1,0),
+        product_tablet = ifelse(product_code == 'q',1,0))
 
 glimpse(my_data)
 
